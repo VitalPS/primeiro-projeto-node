48d685956e3b44ae67af6b5ca0c5af7db91f77b6
Patrik da Silva Vital <patrikdasilvavital@gmail.com>
2020-09-29 19:05:38 -0300

    validando data

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,17 +1,37 @@
 import { request, response, Router } from 'express';
 import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
+// startOfHour -> converte minuto, segundo, milissegundo para zero
+// parseISO -> converte string para formato de data nativo do JS
+// isEqual -> verifica se duas datas são iguais
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+interface appointment {
+  id: string
+  provider: string;
+  date: Date;
+}
+
+const appointments: appointment[] = []; //appointments é declarado como um array de appointment
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentIsEqual = appointments.find(appointment =>
+    isEqual(parsedDate, appointment.date),
+  );
+
+  if (findAppointmentIsEqual) {
+    return response.status(400).json({ message: "This appointment is already booked" })
+  }
+
   const appointment = {
     id: uuid(),
     provider,
-    date,
+    date: parsedDate,
   };
 
   appointments.push(appointment);
