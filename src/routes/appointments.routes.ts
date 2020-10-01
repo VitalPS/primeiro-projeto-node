0cd0ffb1acdbf1e72fe5987e8e2b2aa99e85acb5
Patrik da Silva Vital <patrikdasilvavital@gmail.com>
2020-10-01 12:00:02 -0300

    Services & SOLID

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,10 +1,8 @@
 import { request, response, Router } from 'express';
-import { startOfHour, parseISO } from 'date-fns';
-// startOfHour -> converte minuto, segundo, milissegundo para zero
-// parseISO -> converte string para formato de data nativo do JS
-// isEqual -> verifica se duas datas são iguais
+import { parseISO } from 'date-fns';
 
-import AppointmentsRepository from "../repositories/AppointmentsRepository"
+import AppointmentsRepository from "../repositories/AppointmentsRepository";
+import CreateAppointmentService from "../services/CreateAppointmentService"
 
 const appointmentsRouter = Router();
 const appointmentsRepository = new AppointmentsRepository();
@@ -16,24 +14,21 @@ appointmentsRouter.get('/', (request, response) => {
 })
 
 appointmentsRouter.post('/', (request, response) => {
-  const { provider, date } = request.body;
+  try {
+    const { provider, date } = request.body;
 
-  const parsedDate = startOfHour(parseISO(date));
+    const parsedDate = parseISO(date);
+    // apenas transforma string para data (não é regra de negócio)
+    const createAppointment = new CreateAppointmentService(
+      appointmentsRepository);
 
-  const findAppointmentsInSameDate = appointmentsRepository.findByDate(parsedDate);
+    const appointment = createAppointment.execute({ provider, date: parsedDate });
 
-  if (findAppointmentsInSameDate) {
-    return response.
-      status(400).
-      json({ message: "This appointment is already booked" });
+    return response.json(appointment);
+
+  } catch (err) {
+    return response.status(400).json({ message: err.message });
   }
-
-  const appointment = appointmentsRepository.create({
-    provider,
-    date: parsedDate
-  });
-
-  return response.json(appointment);
 })
 
 export default appointmentsRouter;
